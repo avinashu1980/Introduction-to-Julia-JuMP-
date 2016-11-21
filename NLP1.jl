403c55a7ce9f8c4f86664dd525c81005ab3ce38c
Avinash Unnikrishnan <avinashu@gmail.com>
2016-11-20 18:00:46 -0800

    Add files via upload

--- NLP1.jl
+++ NLP1.jl
@@ -1,11 +1,13 @@
 # Solving Example 10.5.5 in 
 # Mokhtar S. Bazaraa, Hanif D. Sherali, C. M. Shetty. Nonlinear Programming: Theory and Algorithms, 2006
 
-using JuMP
+using JuMP, Ipopt
+m = Model(solver=IpoptSolver())
+
 m = Model()
 @variable(m, x >=0)
 @variable(m, y >=0)
-@objective(m, Min, 2x^2 + 2y^2 - 2x*y - 4x - 6y)
+@NLobjective(m, Min, 2x^2 + 2y^2 - 2x*y - 4x - 6y)
 @constraint(m, const1,  x + y <=  2)
 @constraint(m, const2,  x + 5y <= 5)
 
